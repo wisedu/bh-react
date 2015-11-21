c1f7e501c3a233692e02fe11e92cc2770ec51d3f
贪吃牛仔裤 <445702874@qq.com>
2015-11-21 16:23:35 +0800

    package.son

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry : {
+        tree : ['./src/tree/examples/tree.js']
+    },
+    output : {
+        //path: './dist/',
+        filename: '[name].js'
+    },
+    module : {
+        loaders : [
+            {
+                test : [/\.js$/, /\.jsx$/],
+                loader : ['babel-loader'],
+                exclude : /node-modules/,
+                query : {
+                    presets : ['es2015','react']
+                }
+            },
+            {
+                test : /\.css$/,
+                loaders : ['style', 'css']
+            },
+            {
+                test : /\.less$/,
+                loaders : ['style', 'css', 'less-loader']
+            },
+            {
+                test : /\.scss$/,
+                loaders : ['style', 'css', 'sass-loader']
+            }
+        ]
+    },
+    devtool : 'source-map'
+};
