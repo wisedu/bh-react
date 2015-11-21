7248f2d0003d128e9985c4ad947f90fb426ad919
guduziye <05123875lgh@163.com>
2015-11-21 16:29:53 +0800

    11111

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
+var entry = {};
+entry["button"] = ["./src/button.js"];
+
+module.exports = {
+  entry: entry,
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query:{
+        "compact": false,
+        "presets": ["react", "es2015", "stage-0", "stage-1", "stage-2", "stage-3"]
+      }
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.less$/,
+      loader: ExtractTextPlugin.extract(
+          'style-loader',"css-loader!less-loader"
+      )
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract(
+          'style-loader', 'css-loader'
+      )
+    }]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devtool: 'source-map'
+};
