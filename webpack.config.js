b1d15867d7f5d125fb0c60be2dfc488cf6d71174
guduziye <05123875lgh@163.com>
2015-11-23 12:00:49 +0800

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 
 var entry = {};
-entry["button"] = ["./src/button.js"];
+entry["button"] = ["./src/button/index.jsx"];
 
 module.exports = {
   entry: entry,
