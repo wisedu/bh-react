28b62bd2133bc9e283a6dd1b01c7f96f08f44d38
guduziye <05123875lgh@163.com>
2015-11-26 13:35:48 +0800

    no message

--- src/button/index.jsx
+++ src/button/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button';
+import Button from './button.jsx';
 //import ButtonGroup from './button-group';
 //
 //Button.Group = ButtonGroup;
