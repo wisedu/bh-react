f669f373241bf44eb59d071d1f96dd141a34e14d
Elvis <shuaiqy@hotmail.com>
2015-11-27 11:09:39 +0800

    校验，未完成

--- samples/form.jsx
+++ samples/form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "../src/form/form.scss";
-import {Form, Input, Button, Checkbox, Radio, Row, Col, message} from '../index.js';
+import {Form, Input, Button, Checkbox, Radio, Row, Col, Validation} from '../index.js';
 const FormItem = Form.Item;
 const FormEMAP = Form.MappingEMAP;
 const RadioGroup = Radio.Group;
@@ -13,33 +13,26 @@ var items = formEMAP.getItems("ddrzcx", 2);
 const Demo = React.createClass({
   mixins: [Form.ValueMixin],
 
-  getInitialState() {
-    return {
-      formData: {
-        userName: '大眼萌 minion',
-        password: undefined,
-        gender: 'male',
-        remark: undefined,
-        agreement: undefined,
-      }
-    };
-  },
-
   handleSubmit(e) {
     e.preventDefault();
-    message.success("收到表单值~~~ ：" + JSON.stringify(this.state.formData, function(k, v) {
-      if (typeof v === 'undefined') {
-        return '';
+    const validation = this.refs.validation;
+    validation.validate((valid) => {
+      if (!valid) {
+        console.log('error in form');
+        return;
+      } else {
+        console.log('submit');
       }
-      return v;
-    }));
+      console.log(this.state.formData);
+    });
   },
 
   render() {
-    const formData = this.state.formData;
     return (
       <Form className="bh-fm-compact edit" horizontal cols="2">
-        {items}
+        <Validation ref="validation" onValidate={this.handleValidate}>
+          {items}
+        </Validation>
         <Row>
           <Col span="4" offset="8">
             <Button type="primary" onClick={this.handleSubmit}>确定</Button>
